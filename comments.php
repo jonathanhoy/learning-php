656943df19fa3268eb83bb15d7a171ed7fd821b6
jonathanhoy <jonathanhoy5@gmail.com>
2019-01-15 22:27:30 -0500

    added practice files, startingg section 2

---
+++ comments.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+		// Single line comment
+	
+		/* Multi-line
+			 Comment */
+	
+		// This function below displays text
+		echo 'Hello Student';
+	?>
+</body>
+</html>
